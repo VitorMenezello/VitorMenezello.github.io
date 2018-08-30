3b6890b5068de30547b2da667b2fdd90449cba93
Vitor Menezello <vitormenezello@gmail.com>
2018-08-30 17:29:34 -0300

    Basic single page application attempt.

--- app.js
+++
@@ -1,4 +0,0 @@
-angular.module('App', ['ngMaterial', 'ngMessages'])
-.controller('GitHubController', function($scope) {
-	$scope.test = 'Hello World';
-});
---
+++ app/app.js
@@ -0,0 +1,24 @@
+angular.module('App', ['ngMaterial', 'ngMessages'])
+
+.config(function($routeProvider, $locationProvider) {
+   $locationProvider.html5Mode(true);
+
+   $routeProvider
+   .when('/', {
+      templateUrl : 'app/views/home.html',
+      controller     : 'HomeCtrl',
+   })
+   .when('/about', {
+      templateUrl : 'app/views/about.html',
+      controller  : 'AboutCtrl',
+   })
+   .when('/work', {
+      templateUrl : 'app/views/work.html',
+      controller  : 'WorkCtrl',
+   })
+   .when('/contact', {
+      templateUrl : 'app/views/contact.html',
+      controller  : 'ContactCtrl',
+   })
+   .otherwise ({ redirectTo: '/' });
+});
